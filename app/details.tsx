54a3dc0a305f78b395e3b489718a9ed9d7a43555
JohanTran <johan.tran0210@gmail.com>
2024-10-20 13:25:01 +0200

    init project

---
+++ app/details.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Details() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>testing details site</Text>
+        </View>
+    );
+}
