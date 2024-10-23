ac9d6c8e8c7ea91bd808d69f42e282cbb528f9a9
JohanTran <johan.tran0210@gmail.com>
2024-10-23 23:05:45 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "eslint:recommended", "plugin:react-hooks/recommended"],
+  env: {
+    "node": true,
+    "react-native/react-native": true
+  },
+  plugins: ['react-hooks', "react", "react-native", "react-refresh"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    }
+  },
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+};
