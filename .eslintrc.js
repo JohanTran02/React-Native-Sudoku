542600a8e0c0b24b1b299b584d79cde429ba7fd5
JohanTran <johan.tran0210@gmail.com>
2024-12-25 16:43:11 +0100

    added global.d.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "eslint:recommended", "plugin:react-hooks/recommended"],
+  extends: ["expo", "eslint:recommended", "plugin:react-hooks/recommended", "prettier"],
   env: {
     "node": true,
     "react-native/react-native": true
   },
-  plugins: ['react-hooks', "react", "react-native", "react-refresh"],
+  plugins: ['react-hooks', "react", "react-native", "react-refresh", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,6 +15,7 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": "warn",
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    'prettier/prettier': 'error',
   }
 };
