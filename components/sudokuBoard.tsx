d6798d53a0157c0fa52c7a82342e96f9da036ded
JohanTran <johan.tran0210@gmail.com>
2024-12-26 18:53:25 +0100

    temp bottom sheet fixed #1

--- components/sudokuBoard.tsx
+++ components/sudokuBoard.tsx
@@ -9,42 +9,25 @@ export default function SudokuBoard({ board, playerPos, setPlayerPos }: {
 }) {
     const [currentNumber, setCurrentNumber] = useState<string>("-");
     return (
-        <View>
-            <FlatList className="flex-1" data={board} renderItem={
-                ({ item, index }) => {
-                    const rowIndex = index;
-                    return (
-                        <View className="flex-row">
-                            {item.map((cell, cellIndex) => (
-                                <SudokuCell
-                                    key={cellIndex}
-                                    cell={cell}
-                                    rowIndex={rowIndex}
-                                    cellIndex={cellIndex}
-                                    playerPos={playerPos}
-                                    currentNumber={currentNumber}
-                                    setCurrentNumber={setCurrentNumber}
-                                    setPlayerPos={setPlayerPos}
-                                />
-                            ))}
-                        </View>)
-                }}>
-            </FlatList>
-        </View>
+        <FlatList className="flex-1" data={board} renderItem={
+            ({ item, index }) => {
+                const rowIndex = index;
+                return (
+                    <View className="flex-row">
+                        {item.map((cell, cellIndex) => (
+                            <SudokuCell
+                                key={cellIndex}
+                                cell={cell}
+                                rowIndex={rowIndex}
+                                cellIndex={cellIndex}
+                                playerPos={playerPos}
+                                currentNumber={currentNumber}
+                                setCurrentNumber={setCurrentNumber}
+                                setPlayerPos={setPlayerPos}
+                            />
+                        ))}
+                    </View>)
+            }}>
+        </FlatList>
     )
 }
-// {board.map(((row, rowIndex) => (
-//     <View key={rowIndex} className="flex-row">
-//         {row.map((cell, cellIndex) => (
-//             <SudokuCell
-//                 key={cellIndex}
-//                 playerPos={playerPos}
-//                 setPlayerPos={setPlayerPos}
-//                 cellIndex={cellIndex}
-//                 cell={cell}
-//                 rowIndex={rowIndex}
-//                 currentNumber={currentNumber}
-//                 setCurrentNumber={setCurrentNumber} />
-//         ))}
-//     </View>
-// )))}
