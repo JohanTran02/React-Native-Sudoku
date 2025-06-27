4c35f11ec1ec3a171084910dc2acb52cbd25e4e6
JohanTran <johan.tran0210@gmail.com>
2025-06-27 14:16:05 +0200

    Added eslint and removed nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,9 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: ['react-native-reanimated/plugin']
-  };
+export default function (api) {
+    api.cache(true);
+    return {
+        presets: [
+            ['babel-preset-expo'],
+        ],
+        plugins: ['react-native-reanimated/plugin'],
+    };
 };
